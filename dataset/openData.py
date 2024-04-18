2a4c6cf0ca3f2af91f94ef73f12ec0be6fdd6649
Peter Traver <pdt9929@cm045.hpc.nyu.edu>
2024-04-18 10:29:26 -0400

    Backup plans

--- dataset/openData.py
+++ dataset/openData.py
@@ -1,6 +1,5 @@
 import pickle
 import numpy as np
-from openstl.utils import show_video_line
 
 def open_data(directory):
     with open(directory + 'train.pkl', 'rb') as f:
@@ -22,8 +21,3 @@ def open_data(directory):
     print('Hidden shape: ' + str(np.shape(X_hidden)))
     
     return X_train, Y_train, X_train_mask.transpose(0,2,1,3,4), Y_train_mask.transpose(0,2,1,3,4), X_val, Y_val, X_val_mask.transpose(0,2,1,3,4), Y_val_mask.transpose(0,2,1,3,4), X_hidden
-
-
-# show the given frames from an example
-# example_idx = 0
-# show_video_line(X_train[example_idx], ncols=11, vmax=0.6, cbar=False, out_path=None, format='png', use_rgb=True)
